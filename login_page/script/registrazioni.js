5db7b9ff2b5e01882e8e520f3dcbdbfff29fd89f
nicolofajette <nicolo.fajette@outlook.it>
2017-03-26 21:58:37 +0200

    eliminazione bootstrap da registrazioene e alcune modifice agli altri file

--- login_page/script/registrazioni.js
+++ login_page/script/registrazioni.js
@@ -12,6 +12,8 @@ function validate(){
 	var psw1 = document.getElementById("psw1").value;
 	var psw2 = document.getElementById("psw2").value;
 	
+	var msg = document.getElementById("msg");
+	
 	var pattern_email = /[a-z0-9.]{2,}[@][a-z.]{1,}[.][a-z]{2,}/;	//Pattern e-mail
 	//var pattern_data_old = /[0-3][0-9][/][0-1][0-9][/][1,2][0,9][0-9][0-9]/;	//Pattern data
 	var pattern_data = /[1,2][0,9][0-9][0-9][-][0-1][0-9][-][0-3][0-9]/;
@@ -20,70 +22,57 @@ function validate(){
 	
 	if(nome == ""){
 		alert("Campo nome vuoto");
-		return false;
 	}
 	
 	if(cognome = ""){
 		alert("Campo cognome vuoto");
-		return false;
 	}
 	
 	if(data == ""){
 		alert("Campo data vuoto");
-		return false;
 	}
 	
 	if(luogo == ""){
 		alert("Campo luogo vuoto");
-		return false;
 	}
 	
 	if(indirizzo == ""){
 		alert("Campo indirizzo vuoto");
-		return false;
 	}
 	
 	if(cap == ""){
 		alert("Campo cap vuoto");
-		return false;
 	}
 	
 	if(email == ""){
 		alert("Campo e-mail vuoto");
-		return false;
 	}
 	
 	if(pattern_email.test(email) == false){
-		alert("Formato email non valido");
-		return false;
+		alert("Attenzione: email non valida");
+		document.getElementById("psw1").value = "";
+		document.getElementById("psw2").value = "";
 	}
 	
 	if(sesso == ""){
 		alert("Sei un maschietto o una femminuccia?");
-		return false;
 	}
 	
 	if(username == ""){
 		alert("Campo unsername vuoto");
-		return false;
 	}
 	
 	if(psw1 == ""){
 		alert("Campo password vuoto");
-		return false;
 	}
 	
 	if(psw2 == ""){
 		alert("Campo conferma password vuoto");
-		return false;
 	}
 	
 	if(psw1 != psw2){
-		alert("Le due password non coincidono");
-		return false;
-	}
-	
-	alert("FORM valido");
-	
+		alert("Campi password non coincidono");
+		document.getElementById("psw2").value = "";
+	}	
 	return false;
 }
